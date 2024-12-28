d1c193bee369d8a608c7abf40fd200ca9a3e0ca5
ronaknetwallet <160584184+ronaknetwallet@users.noreply.github.com>
2024-12-28 12:23:02 +0530

    .vs code changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,6 +7,9 @@ function App() {
 
   return (
     <div>
+      <h1>
+      
+      </h1>
 
     </div>
   )
